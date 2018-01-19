5d2c6107cedbaef0ef76fd6a1983dafaf02d86a0
f <839322915@qq.com>
2018-01-19 08:26:53 +0800

    add CRUD category

--- app/controllers/bicycles_controller.rb
+++ app/controllers/bicycles_controller.rb
@@ -3,9 +3,18 @@ class BicyclesController < ApplicationController
   end
 
   def new
+    @bicycle = Bicycle.new
+    @categories = Category.all
   end
 
   def create
+    @bicycle = Bicycle.new(bicycle_params)
+    if @bicycle.save
+      redirect_to bicycle_path(@bicycle)
+    else
+      logger.debug "catch create error"
+      render "new"
+    end
   end
 
   def edit
@@ -16,5 +25,11 @@ class BicyclesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def bicycle_params
+    params.require(:bicycle).permit(:name, :info, :price)
   end
 end
