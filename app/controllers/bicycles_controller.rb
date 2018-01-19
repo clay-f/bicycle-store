beb0d30c9e49bf80d84d8918662f5afe932dc31b
f <839322915@qq.com>
2018-01-19 08:55:32 +0800

    add bicycle CRUD

--- app/controllers/bicycles_controller.rb
+++ app/controllers/bicycles_controller.rb
@@ -1,14 +1,19 @@
 class BicyclesController < ApplicationController
+  before_action :find_bicycle, only: [:show, :edit, :update, :destroy]
+
   def index
+    @bicycles = Bicycle.all
   end
 
   def new
     @bicycle = Bicycle.new
-    @categories = Category.all
+    @categories = Category.all.map { |i| [i.name, i.id] }
   end
 
   def create
     @bicycle = Bicycle.new(bicycle_params)
+    @categories = Category.all.map { |i| [i.name, i.id] }
+
     if @bicycle.save
       redirect_to bicycle_path(@bicycle)
     else
@@ -17,19 +22,30 @@ class BicyclesController < ApplicationController
     end
   end
 
+  def show
+  end
+
   def edit
+    @categories = Category.all.map { |i| [i.name, i.id] }
   end
 
   def update
-    
+    @bicycle.update(bicycle_params)
+    redirect_to bicycle_path(@bicycle)
   end
 
   def destroy
+    @bicycle.destroy
+    redirect_to bicycles
   end
 
   private
 
   def bicycle_params
-    params.require(:bicycle).permit(:name, :info, :price)
+    params.require(:bicycle).permit(:name, :info, :price, :category_id)
+  end
+
+  def find_bicycle
+    @bicycle = Bicycle.find(params[:id])
   end
 end
