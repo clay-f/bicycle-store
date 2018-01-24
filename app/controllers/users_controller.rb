24058c7e9527f96d5e8dc59d6142ff822511c23b
f <839322915@qq.com>
2018-01-24 11:06:08 +0800

    add user authenticate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,10 @@ class UsersController < ApplicationController
   def login
     user_name = params.fetch(:user)[:name]
     passwd = params.fetch(:user)[:password]
-    user = User.find_by(name: user_name, password_digest: passwd)
-    if user
+    user = User.find_by(name: user_name)
+    if user.password == passwd
       session[:user_id] = user.id
-      redirect_to root_url, notice: 'login succeed.'
+      redirect_to root_url, notice: 'welcome, login succeed.'
     else
       redirect_to login_url, notice: 'username or password is invalid.'
     end
@@ -18,7 +18,6 @@ class UsersController < ApplicationController
     sessino[:user_id] = nil
     redirect_to root_url
   end
-
 
   private
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def user_login?
+    if session[:user_id]
+      true
+    else
+      false
+    end
+  end
 end
