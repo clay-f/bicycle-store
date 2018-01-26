d64303ddcd174ddebe3ab71f598355d45af3f315
f <839322915@qq.com>
2018-01-26 22:57:29 +0800

    add store index carousel

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,6 +3,7 @@ class StoreController < ApplicationController
   before_action :set_cart
   
   def index
+    @carousel_products = Product.order("random()").limit(5)
     @products = Product.all
     @categories = Category.all
   end
